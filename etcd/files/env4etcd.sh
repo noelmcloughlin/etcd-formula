71bbb29c75d7e49795cb1a854ed4e608ebee7443
N <noel.mcloughlin@gmail.com>
2018-10-10 12:57:38 +0100

    Fix auto-tls (and kitchen tests).

--- etcd/files/env4etcd.sh
+++ etcd/files/env4etcd.sh
@@ -1,9 +1,11 @@
-{% for key,value in etcd.service.items() -%}
-export ETCD_{{ key|string|upper }}={{ "value" or "" }}
+#ignore --param bools in environ
+{% for key,value in etcd.service.items() if value not in (True, False) -%}
+export ETCD_{{ key|string|upper }}={{ value or "" }}
 {% endfor %}
 
-{% for key,value in etcd.etcdctl.items() -%}
-export ETCDCTL_{{ key|string|upper }}={{ "value" or "" }}
+#ignore --param bools in environ
+{% for key,value in etcd.etcdctl.items() if value not in (True, False) -%}
+export ETCDCTL_{{ key|string|upper }} {{ value or "" }}
 {% endfor %}
 
 export ETCD_HOME={{ etcd.realhome }}
